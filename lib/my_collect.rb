5b537515535dc2ab8eeb56306de66d8582522b98
Tyler Young <tyoung900@gmail.com>
2020-10-07 21:31:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,14 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
 
-def my_collect(array) do |name|
+my_collect(array) do |name|
   name.split(" ").first
 end
